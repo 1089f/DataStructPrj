d4902c3e0ddb660dd19bbb212a2f671ec3897e53
yousef2222587421 <81144264+yousef2222587421@users.noreply.github.com>
2026-08-06 15:47:55 +0300

    Add files via upload

--- src/Events/Leave.cpp
+++ src/Events/Leave.cpp
@@ -2,5 +2,9 @@
 #include "../../include/Simulation/Clinic.h"
 
 Leave::Leave(int ts, int id) : Event(ts), id(id) {}
-void Leave::Execute(Clinic& clinic) { clinic.handleLeave(id); }
+
+void Leave::Execute(Clinic& clinic) {
+    clinic.handleLeave(id);
+}
+
 int Leave::getId() const { return id; }
