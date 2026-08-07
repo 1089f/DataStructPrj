065bf9a0a47b638e3314534fc2ec02a469ea776c
yousef2222587421 <81144264+yousef2222587421@users.noreply.github.com>
2026-08-07 12:57:07 +0300

    Add files via upload

--- include/GUI/GUIrender.h
+++ include/GUI/GUIrender.h
@@ -9,6 +9,20 @@ public:
     void DrawDoctorBox(Doctor* doc, int x, int y);
     void DrawLegend(int y);
     void Shutdown();
+
+    void DrawStatsStrip(int x, int y);
+    void DrawWaitingQueues(int panelX, int panelY, int panelH);
+    void DrawPlaybackControls(int y);
+
 private:
     Clinic& clinic;
+    bool paused;
+    int speedLvl;
+    float fTimer;
+    bool simFinished;
+
+    int getTotalWaitE() const;
+    int getTotalWaitR() const;
+    int getTotalInVisit() const;
+    bool btnClicked(int bx, int by, int bw, int bh) const;
 };
